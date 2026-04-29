1a9cc7361422047bb31276a5a69188766ad230fc
Sam Li <sli262@illinois.edu>
2026-04-29 22:42:55 +0000

    driver init

--- src/driver.cc
+++ src/driver.cc
@@ -1,10 +1,37 @@
 #include <iostream>
 
+#include "GraphAdjList.hpp"
+#include "queue.hpp"
 #include "solution.hpp"
+#include "stack.hpp"
 
 int main() {
-  
-  std::cout << "Hello! This compiles ok." << std::endl;
+  GraphAdjList<char, int> g;
+  g.AddVertex('A');
+  g.AddVertex('B');
+  g.AddVertex('C');
+  g.AddVertex('D');
+  g.AddVertex('E');
+  g.AddVertex('F');
 
-  return 0;
+  g.AddEdge('A', 'B', 1);
+  g.AddEdge('B', 'C', 1);
+  g.AddEdge('B', 'D', 1);
+  g.AddEdge('D', 'E', 1);
+
+  Queue<char> q;
+  Stack<char> s;
+  std::vector<char> out1 = GetConnected(g, 'A', q);
+  std::vector<char> out2 = GetConnected(g, 'A', s);
+
+  std::cout << "BFS output:" << std::endl;
+  for (char c : out1) {
+    std::cout << c << " ";
+  }
+  std::cout << std::endl;
+  std::cout << "DFS output:" << std::endl;
+  for (char c : out2) {
+    std::cout << c << " ";
+  }
+  std::cout << std::endl;
 }
